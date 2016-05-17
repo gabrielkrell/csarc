167a3ffcf61ea87a8cf69f4b6eeb4140f0d827aa
gabrielkrell <gabrielkrell@gmail.com>
2016-05-17 15:57:22 -0500

    ...
    ...

--- csarc/GradientCommand.h
+++ csarc/GradientCommand.h
@@ -8,16 +8,16 @@ extern char* GRADIENT_MODE_TEXTS[];
  
 class GradientCommand {
 	public:
-    	GradientCommand();
-        GradientCommand( int col1[3], int col2[3], GradientMode gradMode, seconds pulselen);
-        GradientCommand( const int col1[3], const int col2[3], GradientMode gradMode, const seconds pulselen);
-        boolean equals(GradientCommand ogc);
+    	GradientCommand( );
+        GradientCommand( int col1[3], int col2[3], GradientMode gradMode, seconds pulselen );
+        GradientCommand( const int col1[3], const int col2[3], GradientMode gradMode, const seconds pulselen );
+        boolean equals(GradientCommand ogc );
         void setCol1( int col1[3] );
         void setCol2( int col2[3] );
         void setCol1( const int col1[3] );
         void setCol2( const int col2[3] );
-        void setMode( GradientMode gradMode);
-        void setPulse( seconds pulselen);
+        void setMode( GradientMode gradMode );
+        void setPulse( seconds pulselen );
         void print();
 
 		int col1[3];
@@ -26,7 +26,7 @@ class GradientCommand {
 		milliseconds pulselen;
 
     private:
-    static void printA( int arr[3]) {
+    static void printA( int arr[3] ) {
       for(int i = 0; i < 3; i++) {
         Serial.print(arr[i]);
         Serial.print(" ");
